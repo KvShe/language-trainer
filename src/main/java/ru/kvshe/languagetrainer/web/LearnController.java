6f120dde6090741e45a91881f857372163986308
Kv <EvgeniyShevyakov@outlook.com>
2024-10-24 11:07:36 +0300

    add dockerfile

--- src/main/java/ru/kvshe/languagetrainer/web/LearnController.java
+++ src/main/java/ru/kvshe/languagetrainer/web/LearnController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ru.kvshe.languagetrainer.model.Word;
 import ru.kvshe.languagetrainer.service.LearnService;
-import ru.kvshe.languagetrainer.service.WordService;
 
 import java.util.Collections;
 import java.util.List;
